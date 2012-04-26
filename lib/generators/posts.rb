116d63ce3b31eaeec1fa7d3435e20d1f7f66367a
Zach Bruhnke <z@zbruhnke.com>
2012-04-26 14:23:54 -0700

    new generator

---
+++ lib/generators/posts.rb
@@ -0,0 +1,22 @@
+class Jekyll::BlogGenerator < Rails::Generators::NamedBase
+    source_root File.expand_path('../jekyll/blog/templates', __FILE__)
+
+  def create_blog source = 'config/jekyll', destination = 'public/blog'
+    options = {
+        'source' => source,
+        'destination' => destination
+    }
+    source_dir = 'config/jekyll'
+    options = Jekyll.configuration(options)
+    site = Jekyll::Site.new(options)
+    
+    begin
+      site.process
+    rescue Jekyll::FatalException => e
+      puts
+      puts "ERROR: YOUR SITE COULD NOT BE BUILT:"
+      puts "------------------------------------"
+      puts e.message
+      exit(1)
+  end
+end
