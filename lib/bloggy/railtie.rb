c7abc7f1dde2591c5706c736c4d449294fe9557b
Zach Bruhnke <z@zbruhnke.com>
2012-04-27 13:38:34 -0700

    all rake tasks

--- lib/bloggy/railtie.rb
+++ lib/bloggy/railtie.rb
@@ -3,7 +3,7 @@ require 'rails'
 module Bloggy
   class Railtie < Rails::Railtie
     rake_tasks do
-      load 'lib/tasks/*.rake'
+      Dir[File.join(File.dirname(__FILE__),'lib/tasks/*.rake')].each { |f| load f }
     end
   end
 end
