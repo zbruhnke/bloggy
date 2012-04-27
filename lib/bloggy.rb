456df0bf9a8571df245824477a642cc4f0a05d83
Zach Bruhnke <z@zbruhnke.com>
2012-04-27 10:40:25 -0700

    require path change

--- lib/bloggy.rb
+++ lib/bloggy.rb
@@ -5,7 +5,7 @@ require "bloggy/version"
 module Jekyll
   module Rails
     module Bloggy
-      require 'bloggy/railtie' if defined?(Rails)
+      require 'lib/bloggy/railtie' if defined?(Rails)
     end
   end
 end
